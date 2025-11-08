ab9d915b4cd5612a985dfef0cb0bfbb9a7fb478c
Fabse120 <fabseH60@gmail.com>
2025-11-08 14:27:44 +0100

    Fixed Typo in class name

--- src/main/java/VoltCast/VoltCastApplication.java
+++ src/main/java/VoltCast/VoltCastApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VoltCastaApplication {
+public class VoltCastApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VoltCastaApplication.class, args);
+		SpringApplication.run(VoltCastApplication.class, args);
 	}
 
 }
