8f7d3870788e51a0208ce27b78eee91aab6d7512
kaiser1101 <kaiser.philipp95@icloud.com>
2025-11-08 14:39:25 +0100

    initial commit

--- src/test/java/VoltCast/VoltCastaApplicationTests.java
+++ src/test/java/VoltCast/VoltCastaApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class VoltCastApplicationTests {
+class VoltCastaApplicationTests {
 
 	@Test
 	void contextLoads() {
