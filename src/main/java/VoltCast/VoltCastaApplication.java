0f2b3893952925f1cab3887d1d1fbf2901240e2a
kaiser1101 <kaiser.philipp95@icloud.com>
2025-11-08 14:20:25 +0100

    initial commit

---
+++ src/main/java/VoltCast/VoltCastaApplication.java
@@ -0,0 +1,13 @@
+package VoltCast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoltCastaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoltCastaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/VoltCast/VoltCastaApplicationTests.java
@@ -0,0 +1,13 @@
+package VoltCast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoltCastaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
