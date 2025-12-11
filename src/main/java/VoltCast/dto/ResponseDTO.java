02f5a744eaea8d85d31e8763631964bd07be332a
laulaobm <laura@diehofers.at>
2025-12-11 11:07:08 +0100

    update: lombok import and inclusion + maven plugin in pom.xml

--- src/main/java/VoltCast/dto/ResponseDTO.java
+++ src/main/java/VoltCast/dto/ResponseDTO.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Component;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-@Component
 public class ResponseDTO {
     private int code;
     private String message;
